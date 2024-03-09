ba3f18d4a69baf8b9751c1d02235f760e23472d3
MirzaFaiz22 <117134699+MirzaFaiz22@users.noreply.github.com>
2024-03-09 21:31:22 +0700

    Delete Praktikum3 directory

--- Praktikum3/JumlahPesananMelebihiStokException.java
+++
@@ -1,7 +0,0 @@
-public class JumlahPesananMelebihiStokException extends Exception {
-      public JumlahPesananMelebihiStokException(String message){
-        super(message);
-      }
-
-    // TO DO 2: Buat konstructor dengan parameter message dan panggil konstruktor parent dengan keyword super
-} 
