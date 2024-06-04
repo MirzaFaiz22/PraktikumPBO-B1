1a9b5fd92e19e155404ea75504eb9e3fbe196afa
MirzaFaiz <117134699+MirzaFaiz22@users.noreply.github.com>
2024-06-04 11:01:00 +0700

    Add files via upload

---
+++ Praktikum11/DiskonLambda.java
@@ -0,0 +1,28 @@
+interface IDiskon {
+    public double hitungDiskon(int harga);
+}
+public class DiskonLambda {
+    public static void main(String[] args) {
+        //tanpa lambda
+        IDiskon diskonMerdeka = new IDiskon() {
+            public double hitungDiskon(int harga) {
+                return harga - (harga * 0.3);
+            }
+        };
+        //dengan lambda
+        IDiskon diskonLebaran = (harga) -> harga - (harga * 0.4);
+        //dengan lambda dengan blok statement
+        IDiskon diskonBiasa = (harga) -> {
+            return harga - (harga * 0.1);
+        };
+        System.out.println("Diskon Merdeka: " + diskonMerdeka.hitungDiskon(45000));
+        System.out.println("Diskon Lebaran: " + diskonLebaran.hitungDiskon(45000));
+        System.out.println("Diskon Biasa: " + diskonBiasa.hitungDiskon(45000));
+    }
+}
+
+/*
+    Diskon Lebaran menggunakan ekpresi sederhana,sedangkan diskon biasa menggunakan block statement 
+    Diskon lebaran adalah lambda sederhana yang langsung mengembalikan hasil dari satu ekspresi
+    Diskon biasa adalah lambda yang menggunakan blok statement untuk memberikan fleksibilitas
+ */
